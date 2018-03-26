c81c3c9534722103faf06c4cd6410b076c875c9a
Fernando Lopes <72784@pmenos.com.br>
2018-03-26 11:21:11 -0300

    Resolvido bug na classe Natureza em permissions

--- NetStandard20/HomesDoc.Core/Entity/Natureza.cs
+++ NetStandard20/HomesDoc.Core/Entity/Natureza.cs
@@ -9,7 +9,7 @@ namespace HomesDoc.Core.Entity
         public bool AllowTags { get; set; }
         public bool ValidationRequired { get; set; }
         public int GrupoId { get; set; }
-        public string Permission { get; internal set; }
+        public string Permission { get; set; }
         public bool AllowCircular { get; set; }
         public int ExpirationDays { get; set; }
         public bool AllowEmptyExpirationDate { get; set; }
